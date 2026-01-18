eadbe797240ad49a21242bc347a38ba073c3ef98
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-17 22:22:09 -0800

    Add logic to handle user input and to check resources

--- src/main.py
+++ src/main.py
@@ -1,5 +1,39 @@
+from data import coffee_flavors, resources
+
+
+def check_resources(actual_resources, flavor):
+    resources_needed = coffee_flavors[flavor].get("ingredients")
+
+    can_make_order = True
+
+    for ingredients, value in resources_needed.items():
+        if actual_resources[ingredients] < value:
+            can_make_order = False
+    return can_make_order
+
+
+
+def menu():
+    print(f"{"*" * 20}Mandalorian Coffee{"*" * 20}")
+    print(f"{"*" * 20}MENU{"*" * 20}")
+
+    for idx, flavor in enumerate(coffee_flavors.keys()):
+        print(f"{idx + 1}. {flavor}")
+
+
 def main():
-    pass
+
+    menu()
+
+    choice = int(input("What you would like today?"))
+
+    all_the_flavors = list(coffee_flavors.keys())
+    chosen_flavor = all_the_flavors[choice - 1]
+
+
+    have_resources = check_resources(resources, chosen_flavor)
+    print(have_resources)
+    
 
 if __name__ == "__main__":
     main()
