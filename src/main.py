8aa91ad18f6f18195ba28c6fee3cd31337a36b0c
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-17 22:37:36 -0800

    Implement report to get actual resources for the coffee machine

--- src/main.py
+++ src/main.py
@@ -9,9 +9,8 @@ def check_resources(actual_resources, flavor):
     for ingredients, value in resources_needed.items():
         if actual_resources[ingredients] < value:
             can_make_order = False
+
     return can_make_order
-
-
 
 def menu():
     print(f"{"*" * 20}Mandalorian Coffee{"*" * 20}")
@@ -20,19 +19,37 @@ def menu():
     for idx, flavor in enumerate(coffee_flavors.keys()):
         print(f"{idx + 1}. {flavor}")
 
+def get_specific_flavor(user_choice):
+    all_the_flavors = list(coffee_flavors.keys())
+    specific_flavor = all_the_flavors[user_choice - 1]
+
+    return specific_flavor
+
+def get_report(actual_resources):
+    print(f"{"*" * 20}Actual resources{"*" * 20}")
+    
+    for ingredients, value in actual_resources.items():
+
+        print(f"{ingredients.capitalize()}: {value}{"mL" if ingredients in ["water", "milk"] else "g"}")
+        
 
 def main():
 
-    menu()
+    while True:
+        menu()
 
-    choice = int(input("What you would like today?"))
+        choice = input("What you would like today?")
 
-    all_the_flavors = list(coffee_flavors.keys())
-    chosen_flavor = all_the_flavors[choice - 1]
+        if choice == "report":
+            get_report(resources)
+            continue
 
 
-    have_resources = check_resources(resources, chosen_flavor)
-    print(have_resources)
+        chosen_flavor = get_specific_flavor(choice)
+
+        have_resources = check_resources(resources, chosen_flavor)
+    
+
     
 
 if __name__ == "__main__":
